b55a449424a0ececf18f85ed694715d08a77b19c
farid <dika59@hotmail.fr>
2016-05-22 15:35:28 +0200

    improvement of the form

--- src/Alfitra/CptBundle/Controller/DonateurController.php
+++ src/Alfitra/CptBundle/Controller/DonateurController.php
@@ -19,45 +19,53 @@ class DonateurController extends Controller
 
     public function incrementAction(Request $request, $id_evnmt)
     {
-    	$em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
 
-    	// var_dump($request->isMethod('POST'));
+        // var_dump($request->isMethod('POST'));
         if(isset($request->request->all()['donateurs'])){
 
-        	$donateur = new Donateurs();
-        	$montant = $request->request->all()['donateurs']['montant'];
-        	isset($montant) && $montant!="" ? : $montant=5;
+            $donateur = new Donateurs();
+            $montant = $request->request->all()['donateurs']['montant'];
+            isset($montant) && $montant!="" ? : $montant=5;
 
-        	$donateur->setMontant($montant);        	
-        	$idCol = intval($request->request->all()['donateurs']['collecteur']);
-	        $collecteur = $em->getRepository('AlfitraCptBundle:Collecteur')->find($idCol);
-	        $prenom = $collecteur->getPrenom();
-	        $evnmt = $em->getRepository('AlfitraCptBundle:Evenement')->find($id_evnmt);
-        	$donateur->setCollecteur($collecteur);
-        	$moyen = isset($request->request->all()['donateurs']['Visa'])?
-        		'Visa':
-        		'Cash';
-        	$donateur->setMoyenDePaiement($moyen);
-        	$donateur->setEvenement($evnmt);
-        	$evnmt->setTotalDonateurs($evnmt->getTotalDonateurs()+1);
-        	$evnmt->setTotalDons($evnmt->getTotalDons()+$montant);
-        	
-        	$moyen == 'Cash' ? $evnmt->setTotalCash($evnmt->getTotalCash()+$montant):
-        					   $evnmt->setTotalCb($evnmt->getTotalCb()+$montant);
+            $donateur->setMontant($montant);            
+            $idCol = intval($request->request->all()['donateurs']['collecteur']);
+            $collecteur = $em->getRepository('AlfitraCptBundle:Collecteur')->find($idCol);
+            $prenom = $collecteur->getPrenom();
+            $evnmt = $em->getRepository('AlfitraCptBundle:Evenement')->find($id_evnmt);
+            $donateur->setCollecteur($collecteur);
+            $moyen = isset($request->request->all()['donateurs']['Visa'])?
+                'Visa':
+                'Cash';
+            $donateur->setMoyenDePaiement($moyen);
+            $donateur->setEvenement($evnmt);
+            $evnmt->setTotalDonateurs($evnmt->getTotalDonateurs()+1);
+            $evnmt->setTotalDons($evnmt->getTotalDons()+$montant);
+            
+            $moyen == 'Cash' ? $evnmt->setTotalCash($evnmt->getTotalCash()+$montant):
+                               $evnmt->setTotalCb($evnmt->getTotalCb()+$montant);
 
-        	$montant > $evnmt->getDonMax() ? $evnmt->setDonMax($montant): null;
-        	$em->persist($donateur);
-        	$em->persist($evnmt);
+            $montant > $evnmt->getDonMax() ? $evnmt->setDonMax($montant): null;
+            $em->persist($donateur);
+            $em->persist($evnmt);
 
-        	$em->flush();
-        	$id_don = $donateur->getId();
-        	$moyenDePaiement = $donateur->getMoyenDePaiement();
-			if ($request->isMethod('POST')) {
-			  $request->getSession()->set("don_id",$id_don);	
-			  $request->getSession()->getFlashBag()->add('notice', 'Le don d\'un montant de '.$montant.' € en '.$moyenDePaiement.' est bien enregistré. Merci à '.$prenom.' .');
-			  return $this->redirectToRoute('challenge');
-			}
+            $em->flush();
+            $id_don = $donateur->getId();
+            $moyenDePaiement = $donateur->getMoyenDePaiement();
+            if ($request->isMethod('POST')) {
+              $request->getSession()->set("don_id",$id_don);    
+              $request->getSession()->getFlashBag()->add('notice', 'Le don d\'un montant de '.$montant.' € en '.$moyenDePaiement.' est bien enregistré. Merci à '.$prenom.' .');
+              return $this->redirectToRoute('challenge');
+            }
         }
+        return $this->render('AlfitraCptBundle:challenge:challenge.html.twig');
+    }
+
+    public function increment2Action($id_evnmt)
+    {
+    	$em = $this->getDoctrine()->getManager();
+
+        
         return $this->render('AlfitraCptBundle:challenge:challenge.html.twig');
     }
 
